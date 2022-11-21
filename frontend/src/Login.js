8ae4f70d78ebbd3235c778e96043ccabe08aa71c
oldborrow <spevak.vlad@gmail.com>
2022-11-21 03:27:06 +0300

    исправлено предупреждение о неверном логине/пароле

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -2,11 +2,12 @@ import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import axios from 'axios';
 import { useNavigate  } from "react-router-dom";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import Header from "./Header";
 
 export default () => {
     const navigate = useNavigate();
+    const [wrongData, useWrongData] = useState(false);
     useEffect(() => {
         if (localStorage.getItem("loggedIn") !== "null" && localStorage.getItem("loggedIn") !== null) {
             navigate("/")
@@ -31,7 +32,7 @@ export default () => {
                     localStorage.setItem("userToken", response.data.token)
                     navigate("/")
                 }).catch((err) => {
-                    alert("Неверные имя пользователя или пароль")
+                    useWrongData(true)
                 })
                 setSubmitting(false)
             }}
@@ -50,6 +51,7 @@ export default () => {
                 <button type="submit">Войти</button>
             </Form>
         </Formik>
+            {wrongData ? <h7>Неверные имя пользователя или пароль</h7> : null}
             <h1>Зарегистрироваться</h1>
             <button onClick={() => navigate("/signup")}>Регистрация</button>
         </>
