aba0452af1a9a8fac73efed15e5938384d018f2f
lowtab <spevak.vlad@gmail.com>
2023-01-22 17:18:50 +0300

    slack

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -4,7 +4,7 @@ import React from 'react';
 function Header() {
   const navigate = useNavigate();
 
-  return <textbox onClick={() => navigate('/')} onKeyDown={() => navigate('/')}>Hexlet Chat</textbox>;
+  return <textbox onClick={() => navigate('/')} onKeyDown={() => navigate('/')}>Slack Chat</textbox>;
 }
 
 export default Header;
